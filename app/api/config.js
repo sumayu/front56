2b728650e0ddd073cea3fb04afbb9d50b6c7fe53
Konstantin Savin <konstantinsavin930@gmail.com>
2024-05-30 05:16:32 +0300

    исправил ссылку в config.js (добавил /api)

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://backend-nevebnavern.nomorepartiesco.ru";
+export const BASE_URL = "https://backend-nevebnavern.nomorepartiesco.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
