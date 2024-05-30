b08fb8958659e30eb18a3b31328824c6aae3619e
Konstantin Savin <konstantinsavin930@gmail.com>
2024-05-30 04:07:48 +0300

    Initial commit

---
+++ app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://backend-nevebnavern.nomorepartiesco.ru";
+
+export const endpoints = {
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/local`,
+  me: `${BASE_URL}/me`,
+};
